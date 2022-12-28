e2a1960367b325ff3af68fec61d22f7d951899d1
0seo8 <00seo23@gmail.com>
2022-12-28 14:02:12 +0900

    visual studio ts settings

---
+++ TS/zero-ts/src/01.js
@@ -0,0 +1,10 @@
+var Language2 = {
+    TypeScript: "TS",
+    JavaScript: "JS",
+    Java: "JAVA",
+    Ruby: "RB"
+};
+function getLang(LangCode) {
+    console.log(LangCode);
+}
+getLang("Java");
