f2273fa28896da892f600eec404d3e3752f3d796
0seo8 <00seo23@gmail.com>
2023-01-01 01:57:16 +0900

    페이지네이션(로딩문제)

--- React-query-udemy/base-blog-em/src/Posts.jsx
+++ React-query-udemy/base-blog-em/src/Posts.jsx
@@ -4,23 +4,27 @@ import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery(더이상 하드 코딩된 배열X)
   //const data = [];
   // 첫번째 인자: key, 두번째인자: 쿼리에 대한 데이터를 가지고 오는 비동기 함수
-  const { data, isError, isLoading, error } = useQuery("post", fetchPosts, {
-    staleTime: 2000,
-  });
+  const { data, isError, isLoading, error } = useQuery(
+    ["post", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
   if (isLoading) return <h3>Loading</h3>;
   if (isError)
     return (
@@ -44,11 +48,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
           Next page
         </button>
       </div>
