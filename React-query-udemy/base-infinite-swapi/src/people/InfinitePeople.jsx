59822c379038c790cf18de7d238ed7accedee6ee
0seo8 <00seo23@gmail.com>
2023-01-01 21:00:53 +0900

    finish useInfiniteQuery

--- React-query-udemy/base-infinite-swapi/src/people/InfinitePeople.jsx
+++ React-query-udemy/base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -8,6 +9,47 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
+  //fetchNextPage: 더 많은 데이터가 필요할 때 어느 함수를 실행할지를 infiniteScroll에 지시
+  //hasNextPage: 수집할 데이터가 더 있는지를 결정하는 불리언
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+  } = useInfiniteQuery(
+    "sw-people",
+    //pageParam: fetchNextPage가 어떻게 보일 지를 결정하고 다음 페이지가 있는지를 결정
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  //만약 isLoading이 아니라 isFetching으로 한다면 게속 스크롤이 최상단으로 이동하는 모습을 볼 수 있음
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
